48b5e94e6242748377c320aaab10bdb4c2d43fc4
Vladislav Tupikin <mrrefactoring@yandex.ru>
2019-10-27 14:33:08 +0300

    bugfixes

--- test/encoder.test.ts
+++ test/encoder.test.ts
@@ -6,4 +6,17 @@ describe('Encoder tests', () => {
 
     expect(encoder).toBeDefined();
   });
+
+  it('should return encoded message', () => {
+    const decoder = new Decoder();
+    const encoder = new Encoder(decoder.publicKey);
+
+    const message = 'message';
+    const encodedMessage = encoder.encode(message);
+
+    const isBigNumberArray = encodedMessage.every(el => typeof el === 'bigint');
+
+    expect(encodedMessage).toHaveLength(message.length);
+    expect(isBigNumberArray).toEqual(true);
+  });
 });
